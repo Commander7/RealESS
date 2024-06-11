e0d183b8a01add3c0a30b2010994d65e395d8158
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-06-11 10:27:21 +0200

    conectionDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();                    // initialize it
+
+mongoose.connect(process.env.MONGODBW).then(() => {                    // check if connected with .then() and well
+    console.log('Connected to MongoDB!');                   
+   }).catch((err) => {
+    console.log(err);
+   })
+
 
 const app = express();
 
