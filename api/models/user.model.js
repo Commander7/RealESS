776e1a9e1e3c631c68014db5c70cc9cf7330965f
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-06-11 11:31:01 +0200

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,                                 // set of rules to user
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, {timestamps: true});
+
+const user = mongoose.model('User', userSchema);                    // create model
+
+
+export default User;
