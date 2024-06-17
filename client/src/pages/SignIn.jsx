d621ffcebb3ae981e0848e968799f1c78cc59b7b
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-06-17 16:01:20 +0200

    Add redux tools

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({}); // check and update/save changes from user
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch(); // initialise function
 
   const handleChange = (e) => {
     // get formdata save userInput with new specific useR values
@@ -17,28 +23,25 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      const res = await fetch("/api/auth/signin", {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin',{
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData), //protect formdata with JSON stringify before submit
+        body: JSON.stringify(formData), //protect formdata with JSON stringify before submit check care string
       });
 
       const data = await res.json(); // respons
+      console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
-
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate("/"); // navigate to home after sign-in
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -48,7 +51,6 @@ export default function SignIn() {
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-     
         <input
           type="email"
           placeholder="email"
