bbad167d8201403432ced6e54b732f5909486253
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-06-26 13:05:32 +0200

    Delete functionallity

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';         // use express for routes function  // define routes in index.js
-import { test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);                  // first router with get method
 router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
 
 export default router;
