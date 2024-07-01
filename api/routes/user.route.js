c8b2f76a0386dd217527dc42ff995f8eb22693b8
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-07-01 15:49:06 +0200

    User Lisitng api Route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';         // use express for routes function  // define routes in index.js
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser, getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,5 @@ const router = express.Router();
 router.get('/test', test);                  // first router with get method
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-
+router.get('/listings/:id', verifyToken, getUserListings)
 export default router;
