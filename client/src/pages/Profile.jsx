2c73de2a9f40416193ef8ee8c92047bde1af3bc0
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-07-03 16:52:01 +0200

    add image slider to listing page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -262,7 +262,7 @@ export default function Profile() {
               key={listing._id}
               className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
-              <Link to={`/listings/${listing._id}`}>
+              <Link to={`/listing/${listing._id}`}>
                 <img
                   src={listing.imageUrls[0]}
                   alt="listing cover"
@@ -271,7 +271,7 @@ export default function Profile() {
               </Link>
               <Link
                 className="flex-1 text-slate-600 font-semibold hover:underline truncate"
-                to={`/listings/${listing._id}`}
+                to={`/listing/${listing._id}`}
               >
                 <p>{listing.name}</p>
               </Link>
