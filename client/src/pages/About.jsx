711df1234e159c700cc2d6e6f94709974edb0f53
Marcello C Bustamante <marcellusvii@hotmail.com>
2024-07-09 11:37:49 +0200

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Bustamante Estates</h1>
+      <p className='mb-4 text-slate-700'>Bustamante Estates is a leading state agency that specializes in helping clients to buy, sell
+        and rent luxury properties in the most desirable lands and areas. Our group of experienced agents
+        are dedicated and proffesional to help you with exceptional service and make your dreams come true.
+        They are with you in the process in the buying and selling with responsability all the way for you to make a great 
+        deal and success.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission is to help our clients achieving their real estate goals by providing expert advice, 
+        personalized service and deep understanding of the local market. Whether you are looking to buy 
+        sell or rent a property, we are here to help you in every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our team of agents has a wealth of experince and knowledge in the real state industry, and we are committed to 
+        providing the highest level of service to our clients. We believe that buying or selling a property should be
+        an exciting and rewarding experience, and we are passionate to making that a reality for each and every one of our
+        clients. 
+      </p>
+    </div>
   )
 }
